ab5ba9092e096c59b44d6f56075ae36508f0e22c
giorgideveloper <43621137+giorgideveloper@users.noreply.github.com>
2020-01-09 17:04:13 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* let persone = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+ 
+let arr = ['plum.png', 'orange.jpg','apple.bmp'];
+console.log(arr[0]);
+
+alert("hello world!");
+let answer = prompt("rogor xar ?", "da");
+console.log(typeof(arr)); */
+
+let incr = 10,
+    decr = 10;
+ incr++;
+ decr--;
+ console.log(incr);
+ console.log(decr);   
